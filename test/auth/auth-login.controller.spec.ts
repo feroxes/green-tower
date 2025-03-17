0ad188d3f60894e0224179dd00bc9e23e5714ee4
Yarik Harmash <yaroslav.harmash@plus4u.net>
2025-03-17 16:29:58 +0200

    auth tests fix

--- test/auth/auth-login.controller.spec.ts
+++ test/auth/auth-login.controller.spec.ts
@@ -1,19 +1,19 @@
 import { TestingModule } from '@nestjs/testing';
-import { AuthService } from '../../src/services/auth/auth.service';
 import { ValidationHelper } from '../validation-helper';
 import { mockDto } from '../mock/mock.dtos';
 import { loginError } from '../../src/api/errors/auth.errors';
 import { createTestModule, clearDatabase, closeDatabaseConnection } from '../test.config';
+import { AuthController } from '../../src/api/controllers/auth.controller';
 
 const CMD = 'auth/login';
 
-describe('AuthLoginService', () => {
-  let service: AuthService;
+describe('AuthLoginController', () => {
+  let controller: AuthController;
   let module: TestingModule;
 
   beforeAll(async () => {
     module = await createTestModule();
-    service = module.get<AuthService>(AuthService);
+    controller = module.get<AuthController>(AuthController);
   });
 
   afterAll(async () => {
@@ -22,29 +22,25 @@ describe('AuthLoginService', () => {
 
   beforeEach(async () => {
     await clearDatabase(module);
-    await service.register(mockDto.authRegisterDto);
-  });
-
-  it('AuthLoginService should be defined', () => {
-    expect(service).toBeDefined();
+    await controller.register(mockDto.authRegisterDto);
   });
 
   describe(CMD, () => {
     it(`${CMD} - HDS`, async () => {
-      const response = await service.login(mockDto.authLoginDto);
+      const response = await controller.login(mockDto.authLoginDto);
       ValidationHelper.auth.validateResponse(response);
     });
 
     it(`${CMD} - wrong email`, async () => {
       const expectedError = loginError.InvalidCredentials();
-      await expect(service.login({ ...mockDto.authLoginDto, email: 'wrongEmail@gmail.com' })).rejects.toThrow(
+      await expect(controller.login({ ...mockDto.authLoginDto, email: 'wrongEmail@gmail.com' })).rejects.toThrow(
         expectedError.message,
       );
     });
 
     it(`${CMD} - wrong password`, async () => {
       const expectedError = loginError.InvalidCredentials();
-      await expect(service.login({ ...mockDto.authLoginDto, password: 'wrongPassword' })).rejects.toThrow(
+      await expect(controller.login({ ...mockDto.authLoginDto, password: 'wrongPassword' })).rejects.toThrow(
         expectedError.message,
       );
     });
--- test/auth/auth-signup.controller.spec.ts
+++ test/auth/auth-signup.controller.spec.ts
@@ -1,25 +1,25 @@
 import { TestingModule } from '@nestjs/testing';
 import { Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { AuthService } from '../../src/services/auth/auth.service';
 import { User } from '../../src/entities/user.entity';
 import { Farm } from '../../src/entities/farm.entity';
 import { ValidationHelper } from '../validation-helper';
 import { mockDto } from '../mock/mock.dtos';
 import { registerError } from '../../src/api/errors/auth.errors';
 import { createTestModule, clearDatabase, closeDatabaseConnection } from '../test.config';
+import { AuthController } from '../../src/api/controllers/auth.controller';
 
 const CMD = 'auth/signup';
 
-describe('AuthSignupService', () => {
-  let service: AuthService;
+describe('AuthController', () => {
+  let controller: AuthController;
   let module: TestingModule;
   let farmRepository: Repository<Farm>;
   let userRepository: Repository<User>;
 
   beforeAll(async () => {
     module = await createTestModule();
-    service = module.get<AuthService>(AuthService);
+    controller = module.get<AuthController>(AuthController);
     farmRepository = module.get<Repository<Farm>>(getRepositoryToken(Farm));
     userRepository = module.get<Repository<User>>(getRepositoryToken(User));
   });
@@ -32,13 +32,9 @@ describe('AuthSignupService', () => {
     await clearDatabase(module);
   });
 
-  it('AuthSignupService should be defined', () => {
-    expect(service).toBeDefined();
-  });
-
   describe(CMD, () => {
     it(`${CMD} - HDS`, async () => {
-      const response = await service.register(mockDto.authRegisterDto);
+      const response = await controller.register(mockDto.authRegisterDto);
 
       const farm = await farmRepository.findOne({
         where: { name: mockDto.authRegisterDto.farmName },
@@ -55,9 +51,9 @@ describe('AuthSignupService', () => {
     });
 
     it(`${CMD} - user already exists error`, async () => {
-      await service.register(mockDto.authRegisterDto);
+      await controller.register(mockDto.authRegisterDto);
       const expectedError = registerError.UserAlreadyExists();
-      await expect(service.register(mockDto.authRegisterDto)).rejects.toThrow(expectedError.message);
+      await expect(controller.register(mockDto.authRegisterDto)).rejects.toThrow(expectedError.message);
     });
   });
 });
