b685652e8bafa0d19212d255185ab6d884fb9377
feroxes <feroxes@gmail.com>
2025-06-09 11:17:50 +0300

    added ReactQueryDevtools

--- green-tower-client/src/core/app/app.tsx
+++ green-tower-client/src/core/app/app.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import BackgroundProvider from '../../components/background-provider/background-provider';
 import ThemeProvider from '../../components/theme-provider/theme-provider';
@@ -27,6 +28,7 @@ function App() {
           </AuthProvider>
         </BackgroundProvider>
       </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
